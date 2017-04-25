d0633ba05e06c18607fc5b0043f5301b189f4518
nathan <smithnathan339@gmail.com>
2017-04-24 17:11:09 -0700

    Fleshed out more stubs, refactored token types from main into Token.h

---
+++ A3Compiler/Grammar.h
@@ -0,0 +1,39 @@
+#pragma once
+// Grammar class holds A3 grammar rules inside an array.
+/* TODOs:
+Parameterized constructor
+Getters/Setters
+Utility Functions
+...
+Add to main driver
+*/
+
+#include "Rule.h"
+class Grammar {
+	Grammar();
+	~Grammar();
+
+	Rule mRules[100];
+	
+	// TODO: Parameterized Constructor
+
+	// TODO: Getters/Setters
+	
+	Rule getRule(int index);		// returns the rule at specified index
+
+	// TODO: Utility Functions
+
+};
+
+
+Grammar::Grammar() 
+{};
+
+Grammar::~Grammar() 
+{};
+
+Rule Grammar::getRule(int index)
+{
+	// Params: index. Returns Rule object at specified index
+	return mRules[index];
+}
